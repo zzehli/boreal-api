75ee5723060286fb255215a81f5d00e6e912dbb1
jl <jaeli_ottawa@outlook.com>
2025-05-17 16:37:28 -0400

    feat: working rag

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
-from langchain_core.vectorstores import InMemoryVectorStore
-from typing import Literal
 import os
+from typing import Literal
+
+from langchain_core.vectorstores import InMemoryVectorStore
+
 os.environ['USER_AGENT'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
 
 import bs4
+from dotenv import load_dotenv
 from langchain import hub
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_core.documents import Document
-from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_openai import AzureChatOpenAI, AzureOpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langgraph.graph import START, StateGraph
-from langchain_azure_ai.chat_models import AzureAIChatCompletionsModel
-from langchain_azure_ai.embeddings import AzureAIEmbeddingsModel
 from typing_extensions import Annotated, List, TypedDict
 
-from dotenv import load_dotenv
 load_dotenv()
 embed_model_name = "text-embedding-3-large"
-print(os.environ["AZURE_INFERENCE_ENDPOINT"])
-# https://learn.microsoft.com/en-us/azure/ai-foundry/how-to/develop/langchain#using-azure-openai-models
-# https://github.com/langchain-ai/langchain-azure
-def main():
-    llm = AzureAIChatCompletionsModel(
-        endpoint=os.environ["AZURE_INFERENCE_ENDPOINT"],
-        credential=os.environ["AZURE_INFERENCE_CREDENTIAL"],
-        api_version="2025-01-01-preview",
-        model_name="gpt-4o",
-    )
 
+def main():
+    llm = AzureChatOpenAI(
+        api_version="2024-12-01-preview",
+        model="gpt-4o",
+    )
     # print(llm.invoke("What is the capital of France?"))
-    # llm = init_chat_model("gpt-4o-mini", model_provider="openai")
+    
+    embeddings = AzureOpenAIEmbeddings(
+        model="text-embedding-3-large",
+        )
+    
+    vector_store = InMemoryVectorStore(embeddings)
 
     # Load and chunk contents of the blog
     loader = WebBaseLoader(
@@ -57,12 +57,8 @@ def main():
             document.metadata["section"] = "middle"
         else:
             document.metadata["section"] = "end"
-    print("line 60")
 
-    embed_model = AzureAIEmbeddingsModel(
-        model_name=embed_model_name,
-    )
-    vector_store = InMemoryVectorStore(embed_model)
+
     _ = vector_store.add_documents(all_splits)
 
     
@@ -80,8 +76,6 @@ def main():
     # Define prompt for question-answering
     prompt = hub.pull("rlm/rag-prompt")
 
-    print("line 83")
-
     # Define state for application
     class State(TypedDict):
         question: str
@@ -91,17 +85,7 @@ def main():
 
 
     def analyze_query(state: State):
-        # set tool_choice is a problem
-        llm_with_tools = llm.bind_tools([Search], tool_choice="auto")
-        
-        # Then use an output parser to extract the structured output
-        from langchain_core.output_parsers.openai_tools import JsonOutputKeyToolsParser
-        output_parser = JsonOutputKeyToolsParser(key_name="Search", first_tool_only=True)
-        
-        # Create the chain
-        structured_llm = llm_with_tools | output_parser
-        
-        print(structured_llm)
+        structured_llm = llm.with_structured_output(Search)
         query = structured_llm.invoke(state["question"])
         return {"query": query}
 
@@ -121,11 +105,9 @@ def main():
         response = llm.invoke(messages)
         return {"answer": response.content}
 
-    print("line 114")
 
     graph_builder = StateGraph(State).add_sequence([analyze_query, retrieve, generate])
     graph_builder.add_edge(START, "analyze_query")
-    print("line 117")
 
     graph = graph_builder.compile()
     response = graph.invoke({"question": "What is Task Decomposition?"})
